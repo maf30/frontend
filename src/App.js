6acce07eac7d7acf4cadd1a5b1396293c63426d6
maf <maf67@mail.aub.edu>
2024-01-31 12:50:08 +0200

    added frontend

--- src/App.js
+++ src/App.js
@@ -1,22 +1,32 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import axios from 'axios';
 
 function App() {
+  const [responseData, setResponseData] = useState(null);
+
+  const fetchData = async () => {
+    try {
+      const response = await axios.get('https://localhost:4000/welcome'); // Replace with your API endpoint
+      setResponseData(response.mssg);
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button onClick={fetchData}>Fetch Data</button>
+        {responseData && (
+          <div>
+            <h2>Response Data:</h2>
+            <pre>{JSON.stringify(responseData, null, 2)}</pre>
+          </div>
+        )}
       </header>
     </div>
   );
